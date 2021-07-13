24d5941e6d2055dbda36354d0d7d3ed52d4f0802
Adrienne Bienapfl <adrienne.bienapfl@fortifiedhq.com>
2021-07-13 13:27:24 -0600

    favicon test

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,18 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Electric Adriland",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'React go',
+        short_name: 'Reactgo',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'src/images/favicon-16x16.png',      },
+    }
+  ],
 };
